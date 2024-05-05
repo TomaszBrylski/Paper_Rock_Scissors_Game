1bc158f4758e45a2f6ee298061f538fbe239e41c
Tomasz Brylski <brylskitomasz@gmail.com>
2024-05-05 13:32:07 +0200

    added js used let / function

---
+++ script.js
@@ -0,0 +1,22 @@
+let [computer_score,user_score]=[0,0];
+let result_ref = document.getElementById("result");
+let choices_object = {
+    'rock' : {
+        'rock' : 'draw',
+        'scissor' : 'win',
+        'paper' : 'lose'
+    },
+    'scissor' : {
+    'rock' : 'lose',
+    'scissor' : 'draw',
+    'paper' : 'win'
+    },
+    'paper' : {
+       'rock' : 'win',
+       'scissor' : 'lose',
+       'paper' : 'draw' 
+    },
+}
+function checker(input){
+    var choices = ["rock", "paper", "scissor"];
+}
